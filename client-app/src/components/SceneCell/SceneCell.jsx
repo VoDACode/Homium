a699310f7c6cbec3d47b650de197a8e4e16bfaf9
OlehMaliuta <104268973+OlehMaliuta@users.noreply.github.com>
2023-01-31 23:52:53 +0200

    Home page update
    * added default canvas image for SceneCell object.

--- client-app/src/components/SceneCell/SceneCell.jsx
+++ client-app/src/components/SceneCell/SceneCell.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 import cl from './.module.css';
 
-const SceneCell = () => {
+const SceneCell = ({bitmap}) => {
+    const sourceCanvasRef = useRef();
+
+    useEffect(() => {
+        var sourceCanvas = sourceCanvasRef.current;
+        var sourceContext = sourceCanvas.getContext('2d');
+
+        if (bitmap === undefined) {
+            var a1 = 25;
+            var b1 = sourceCanvas.height * 0.8;
+            sourceContext.fillStyle = 'orange';
+            sourceContext.fillRect((sourceCanvas.width - a1) / 2, (sourceCanvas.height - b1) / 2, a1, b1);
+            sourceContext.fillRect((sourceCanvas.width - b1) / 2, (sourceCanvas.height - a1) / 2, b1, a1);
+        }
+        else {
+            //sourceContext.drawImage(bitmap, 0, 0);
+        }
+    }, []);
+
     return (
         <div className={cl.main}>
+            <canvas ref={sourceCanvasRef}>Scene</canvas>
         </div>
     );
 }
--- client-app/src/pages/HomePage.jsx
+++ client-app/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SceneCell from "../components/SceneCell/SceneCell";
 import SceneContainer from "../components/SceneContainer/SceneContainer";
 import TopBar from "../components/TopBar/TopBar";
+import img from './log-out.png';
 
 const HomePage = () => {
     return (
